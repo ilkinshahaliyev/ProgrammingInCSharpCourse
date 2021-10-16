c0e2e65661b1495ee666849e0c5a654ee278d289
Ilkin Shahaliyev <shahaliyev2021@gmail.com>
2021-10-16 22:43:04 +0400

    Updated!

--- Lesson 3/Lesson 3 Taks/Task3/Task3/Program.cs
+++ Lesson 3/Lesson 3 Taks/Task3/Task3/Program.cs
@@ -6,23 +6,16 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            //Console.Write("Enter first number : ");
-            //double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter first number : ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
 
-            //Console.Write("Enter second number : ");
-            //double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
 
-            //double result = firstNumber / secondNumber;
+            double result = firstNumber / secondNumber;
 
-            //Console.WriteLine("Result is : {0}", result);
-            //Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
-
-            Console.Beep();
-            Console.Beep();
-            Console.Beep();
-            Console.Beep();
-
-            Console.Beep();
+            Console.WriteLine("Result is : {0}", result);
+            Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
         }
     }
 }
