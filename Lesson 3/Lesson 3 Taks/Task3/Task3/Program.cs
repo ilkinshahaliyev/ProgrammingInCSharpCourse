0d51b567d90d10df11da453c11b02c5415b48cc5
Ilkin Shahaliyev <shahaliyev2021@gmail.com>
2021-10-16 16:27:36 +0400

    Repository updated!

---
+++ Lesson 3/Lesson 3 Taks/Task3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number : ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double result = firstNumber / secondNumber;
+
+            Console.WriteLine("Result is : {0}", result);
+            Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
+        }
+    }
+}
