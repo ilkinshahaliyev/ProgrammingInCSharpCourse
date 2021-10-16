7955bd7fc0c037b10643a0d94f460af575d46066
Ilkin Shahaliyev <shahaliyev2021@gmail.com>
2021-10-16 15:47:40 +0400

    Repository updated!

---
+++ Lesson 3/Lesson 3 Taks/Task1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter any numbers : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("This is even number.");
+            }
+            else
+            {
+                Console.WriteLine("This is odd number.");
+            }
+        }
+    }
+}
---
+++ Lesson 3/Lesson 3 Taks/Task2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first string input : ");
+            string firstInput = Console.ReadLine();
+
+            Console.Write("Enter second string input : ");
+            string secondInput = Console.ReadLine();
+
+            if (firstInput.Contains(secondInput))
+            {
+                Console.WriteLine("{0} is in {1}", secondInput, firstInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not in {1}", secondInput, firstInput);
+            }
+        }
+    }
+}
